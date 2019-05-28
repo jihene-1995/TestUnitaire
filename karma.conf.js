c8375d7252a6fa78be1a641e8527e507e1572df1
jihene-1995 <jihene37@hotmail.fr>
2019-05-29 00:04:41 +0100

    unitTest

---
+++ karma.conf.js
@@ -0,0 +1,66 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = function(config) {
+  var configuration = {
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-mocha-reporter'),
+      require('karma-html-reporter')
+    ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, 'coverage'), reports: ['html', 'lcovonly'],
+      fixWebpackSourcePaths: true
+    },
+    
+    reporters: ['mocha', 'kjhtml', 'html'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadless'],
+     
+     htmlReporter: {
+      outputDir: 'karma_html', // where to put the reports 
+      templatePath: null, // set if you moved jasmine_template.html
+      focusOnFailures: true, // reports show failures on start
+      namedFiles: false, // name files instead of creating sub-directories
+      pageTitle: null, // page title for reports; browser info by default
+      urlFriendlyName: false, // simply replaces spaces with _ for files/dirs
+      reportName: 'report-summary-filename', // report summary filename; browser info by default
+      
+      
+      // experimental
+      preserveDescribeNesting: false, // folded suites stay folded 
+      foldAll: false, // reports start folded (only with preserveDescribeNesting)
+    }, 
+
+ customLaunchers: {
+      "Chrome-headless": {
+        base: 'ChromeHeadless',
+        flags: [
+          '--headless', 
+          '--remote-debugging-port=9222',
+          '--no-sandbox', 
+          '--proxy-server=\'direct://\'',
+          '--proxy-bypass-list=*'
+        ]
+      }
+    },
+    singleRun: true
+  };
+  if (process.env.TRAVIS) {
+    configuration.browsers = ['ChromeTravisCi'];
+  }
+
+  config.set(configuration);
+};
